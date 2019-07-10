d626b3e8328155d411d2944ec39abc348ed08392
KishoreVS <33390009+KishoreVS@users.noreply.github.com>
2019-07-10 06:16:18 +0530

    Update WalletService.java

--- WalletService.java
+++ WalletService.java
@@ -2,34 +2,34 @@ package com.wallet.service;
 
 public class WalletService implements WalletServiceI {
 
-	public void createAccount() {
-		// TODO Auto-generated method stub
-		System.out.println("");
-	}
-
-	public void balanceEnquiry() {
+	WalletDao wd=new WalletDao();
+	@Override
+	public void balanceEnquiry(String accNo) {
 		// TODO Auto-generated method stub
 		
 	}
-
-	public void deposit() {
+	@Override
+	public void deposit(String accNo, double dep) {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	public void withdrawal(String accNo, double wAmt) {
 		// TODO Auto-generated method stub
 		
 	}
-
-	public void withdrawal() {
+	@Override
+	public void transfer(String accNo, String ran, double tAmt) {
 		// TODO Auto-generated method stub
 		
 	}
-
-	public void transfer() {
-		// TODO Auto-generated method stub
-		
-	}
-
+	@Override
 	public void miniStatement() {
 		// TODO Auto-generated method stub
 		
 	}
-
+	@Override
+	public void createAccount(WalletBean wbCreateAccount) {
+		// TODO Auto-generated method stub
+		
+	}
 }
