2620458f6543313abacf966fc68baa7a110f0860
KishoreVS <33390009+KishoreVS@users.noreply.github.com>
2019-07-10 06:16:57 +0530

    Update WalletServiceI.java

--- WalletServiceI.java
+++ WalletServiceI.java
@@ -1,9 +1,10 @@
 package com.wallet.service;
 
 public interface WalletServiceI {
-	public void createAccount();
-	public void balanceEnquiry();
-	public void withdrawal();
-	public void transfer();
+	public void createAccount(WalletBean wbCreateAccount) ;
+	public void balanceEnquiry(String accNo);
+	public void deposit(String accNo,double dep);
+	public void withdrawal(String accNo,double wAmt);
+	public void transfer(String accNo,String ran,double tAmt);
 	public void miniStatement();
 }
